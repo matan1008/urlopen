06d0e571082ed36aa3a53f48dadc481948077595
matan <matan1008@gmail.com>
2021-03-15 11:04:20 +0200

    Initial Commit

---
+++ urlopen/urlopen.h
@@ -0,0 +1,12 @@
+//
+//  urlopen.h
+//  urlopen
+//
+//  Created by Matan on 3/15/21.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface urlopen : NSObject
+
+@end
---
+++ urlopen/urlopen.m
@@ -0,0 +1,12 @@
+//
+//  urlopen.m
+//  urlopen
+//
+//  Created by Matan on 3/15/21.
+//
+
+#import "urlopen.h"
+
+@implementation urlopen
+
+@end
