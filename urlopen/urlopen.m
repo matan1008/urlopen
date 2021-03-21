b014c752c003ee9f1c2ddf8a6d28e944c138d5ef
matan <matan1008@gmail.com>
2021-03-21 08:36:25 +0200

    Opening URLs now

--- urlopen/urlopen.m
+++ urlopen/urlopen.m
@@ -5,8 +5,30 @@
 //  Created by Matan on 3/15/21.
 //
 
+@import Foundation;
+@import CoreServices;
 #import "urlopen.h"
 
-@implementation urlopen
+
+@interface LSApplicationWorkspace : NSObject
+
+- (int) openURL:(NSURL *)url;
++ (LSApplicationWorkspace *) defaultWorkspace;
 
 @end
+
+
+int main(int argc, const char *argv[]) {
+    @autoreleasepool {
+        if (2 > argc) {
+            NSLog(@"Mission URL paramters");
+            return 1;
+        }
+        id workspace = [LSApplicationWorkspace defaultWorkspace];
+        NSString *argument = [NSString stringWithCString:argv[1] encoding:NSUTF8StringEncoding];
+        NSLog(@"Opening URL: %@", argument);
+        int result = [workspace openURL:[NSURL URLWithString:argument]];
+        NSLog(@"openURL result: %d", result);
+    }
+    return 0;
+}
